9d3e1b21b8d02d20addee124590e081d25aa1df5
nyaoya7 <n.yasugi@gmail.com>
2024-11-10 09:47:13 +0900

    Excercise 2

---
+++ ex_02.php
@@ -0,0 +1,22 @@
+<?php
+/*** 課題5
+ * https://www.php.net/manual/ja/reserved.variables.php
+ */
+
+/*** 課題6
+ * _GET と　_REQUEST
+ */
+
+/*** 課題7
+ * _POST と _PUT
+ */
+
+/*** 課題8
+ */
+
+ define("BAR",     "value 1");
+ define("BAR2",    "value 2");
+ define("BAR_FOO", "value 3");
+
+?>
+
